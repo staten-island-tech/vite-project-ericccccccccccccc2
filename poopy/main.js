7ecd9d06640bb99784ca63edef6e6c111f49e9b7
ericccccccccccccc2 <113203325+ericccccccccccccc2@users.noreply.github.com>
2022-12-06 14:00:54 -0500

    asdf

--- poopy/main.js
+++ poopy/main.js
@@ -1,11 +1,8 @@
 import "./style.css";
 import { pets } from "./wowie";
 
-function objectMaker(element) {
-  let name = pets.element.name;
-  return `<p>${name}</p>`;
-}
-
-document.querySelector("#one").innerHTML = `
-<p>amongus</p>
-`;
+pets.forEach(
+  (element) =>
+    (document.querySelector("#one").insertAdjacentHTML = `<p>${element.name}</p>
+    <img src="${element.url}" alt="">`)
+);
