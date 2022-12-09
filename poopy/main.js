8b5310c9b710b5224f86f9b2a0d76904cab9068f
ericccccccccccccc2 <113203325+ericccccccccccccc2@users.noreply.github.com>
2022-12-09 13:59:31 -0500

    aDFSDF

--- poopy/main.js
+++ poopy/main.js
@@ -2,10 +2,24 @@ import "./style.css";
 import { pets } from "./wowie";
 
 let domdiv = document.getElementById("one");
+let redbutton = document.getElementById("red");
 
 pets.forEach((element) =>
   domdiv.insertAdjacentHTML(
-    "afterend",
-    `<p class = "subtitles">${element.name}, ${element.price} dollars</p> <img class = "image" src="${element.url}" alt="">`
+    "beforeend",
+    `<div class = "help"><img class = "image" src="${element.url}" alt=""> <p class = "subtitles">${element.name}, ${element.price} dollars</p> </div>`
   )
 );
+
+function redColor() {
+  let temp = pets.filter((element) => element.color == "red");
+  domdiv.innerHTML = "";
+  temp.forEach((element) =>
+    domdiv.insertAdjacentHTML(
+      "beforeend",
+      `<div class = "help"><img class = "image" src="${element.url}" alt=""> <p class = "subtitles">${element.name}, ${element.price} dollars</p> </div>`
+    )
+  );
+}
+
+redbutton.addEventListener("click", redColor);
